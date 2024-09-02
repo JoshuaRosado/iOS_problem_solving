237c60fc7ce1bfdbfb3d61793dbd8dfa0bfddd54
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-01 19:45:25 -0500

    //Write a Swift program to test whether the last digit of the two given non-negative integer values are same or not.

--- swift_problem18/swift_problem_18.playground/Contents.swift
+++ swift_problem18/swift_problem_18.playground/Contents.swift
@@ -5,9 +5,14 @@ import Foundation
 //Write a Swift program to test whether the last digit of the two given non-negative integer values are same or not.
 
 
-func checkLastDigit(firstInt: Int, secondInt:Int){
-    let lastDigit = firstInt.words.endIndex
-    print(lastDigit)
+func checkLastDigit(firstInt: Int, secondInt:Int) -> Bool{
+    
+    if firstInt % 10 == secondInt % 10{
+        return true
+    }
+    else{
+        return false
+    }
 }
 
-checkLastDigit(firstInt: 10, secondInt: 12)
+print(checkLastDigit(firstInt: 15, secondInt: 105))
